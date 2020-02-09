4d28ad9b98b190f5529dacb1adbe991c5932cfc6
Tommy Cox <tommy.cox@protonmail.com>
2020-02-08 19:15:34 -0700

    Add Buidler

---
+++ buidler.config.ts
@@ -0,0 +1,10 @@
+import { BuidlerConfig } from '@nomiclabs/buidler/config'
+
+const config: BuidlerConfig = {
+  solc: {
+    version: '0.6.2'
+  },
+  defaultNetwork: 'buidlerevm'
+}
+
+module.exports = config;
